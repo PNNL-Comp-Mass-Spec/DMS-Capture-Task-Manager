e017c5aa5f9f95c9b7ded17d45def1d2d581fd08
Dave Clark <Dave.Clark@pnnl.gov>
2010-05-07 18:19:35 +0000

    Modified DatasetQualityPlugin to run Matt's MS_File_Info_Scanner dll.

--- DatasetQualityPlugin/DatasetQualityPlugin/clsPluginMain.cs
+++ DatasetQualityPlugin/DatasetQualityPlugin/clsPluginMain.cs
@@ -6,9 +6,12 @@
 // Created 10/06/2009
 //
 // Last modified 10/06/2009
+//						05/04/2010 (DAC) - Added Matt Monroe MSFileScanner for quality checks
 //*********************************************************************************************************
 using System;
 using CaptureTaskManager;
+using MSFileInfoScanner;
+using System.IO;
 
 namespace DatasetQualityPlugin
 {
@@ -17,6 +20,16 @@ namespace DatasetQualityPlugin
 		//*********************************************************************************************************
 		// Main class for plugin
 		//**********************************************************************************************************
+
+		#region "Constants"
+			const string MS_FILE_SCANNER_DS_INFO_SP = "CacheDatasetInfoXML";
+		#endregion
+
+		#region "Module variables"
+			clsMSFileInfoScanner m_MsFileScanner;
+			string m_Msg;
+			bool m_ErrOccurred = false;
+		#endregion
 
 		#region "Constructors"
 			public clsPluginMain()
@@ -47,11 +60,13 @@ namespace DatasetQualityPlugin
 				msg = "Performing quality checks for dataset '" + dataset + "'";
 				clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogDb, clsLogTools.LogLevels.INFO, msg);
 
-				msg = "Dataset Quality tool is not presently active";
-				clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogDb, clsLogTools.LogLevels.WARN, msg);
-				retData.EvalCode = EnumEvalCode.EVAL_CODE_NOT_EVALUATED;
-				retData.EvalMsg = msg;
-				retData.CloseoutType = EnumCloseOutType.CLOSEOUT_SUCCESS;
+				//msg = "Dataset Quality tool is not presently active";
+				//clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogDb, clsLogTools.LogLevels.WARN, msg);
+				//retData.EvalCode = EnumEvalCode.EVAL_CODE_NOT_EVALUATED;
+				//retData.EvalMsg = msg;
+				//retData.CloseoutType = EnumCloseOutType.CLOSEOUT_SUCCESS;
+
+				retData = RunMsFileInfoScanner();
 
 				msg = "Completed clsPluginMain.RunTool()";
 				clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.DEBUG, msg);
@@ -72,8 +87,189 @@ namespace DatasetQualityPlugin
 
 				base.Setup(mgrParams, taskParams, statusTools);
 
+				// Initialize MSFileScanner class
+				m_MsFileScanner = new clsMSFileInfoScanner();
+				m_MsFileScanner.ErrorEvent += new clsMSFileInfoScanner.ErrorEventEventHandler(m_MsFileScanner_ErrorEvent);
+				m_MsFileScanner.MessageEvent += new clsMSFileInfoScanner.MessageEventEventHandler(m_MsFileScanner_MessageEvent);
+
 				msg = "Completed clsPluginMain.Setup()";
 				clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.DEBUG, msg);
+			}	// End sub
+
+			/// <summary>
+			/// Runs the MS_File_Info_Scanner tool
+			/// </summary>
+			/// <returns></returns>
+			private clsToolReturnData RunMsFileInfoScanner()
+			{
+				clsToolReturnData result = new clsToolReturnData();
+				string sourceFolder;
+
+				// Init folder paths based on client/server perspective
+				if (m_TaskParams.GetParam("perspective").ToLower() == "server")
+				{
+					sourceFolder = m_TaskParams.GetParam("Storage_Vol");
+				}
+				else
+				{
+					sourceFolder = m_TaskParams.GetParam("Storage_Vol_External");
+				}
+
+				// Set up the rest of the paths
+				sourceFolder = Path.Combine(sourceFolder,m_TaskParams.GetParam("Storage_Path"));
+				sourceFolder = Path.Combine(sourceFolder, m_TaskParams.GetParam("Folder"));
+				string outputFolder = Path.Combine(sourceFolder, "QC");
+
+				// Set up the params for the MS file scanner
+				m_MsFileScanner.DSInfoDBPostingEnabled = false;
+				m_MsFileScanner.SaveTICAndBPIPlots = bool.Parse(m_TaskParams.GetParam("SaveTICAndBPIPlots"));
+				m_MsFileScanner.SaveLCMS2DPlots = bool.Parse(m_TaskParams.GetParam("SaveLCMS2DPlots"));
+				m_MsFileScanner.ComputeOverallQualityScores = bool.Parse(m_TaskParams.GetParam("ComputeOverallQualityScores"));
+				m_MsFileScanner.CreateDatasetInfoFile = bool.Parse(m_TaskParams.GetParam("CreateDatasetInfoFile"));
+				m_MsFileScanner.LCMS2DPlotMZResolution = float.Parse(m_TaskParams.GetParam("LCMS2DPlotMZResolution"));
+				m_MsFileScanner.LCMS2DPlotMaxPointsToPlot = int.Parse(m_TaskParams.GetParam("LCMS2DPlotMaxPointsToPlot"));
+				m_MsFileScanner.LCMS2DPlotMinPointsPerSpectrum = int.Parse(m_TaskParams.GetParam("LCMS2DPlotMinPointsPerSpectrum"));
+				m_MsFileScanner.LCMS2DPlotMinIntensity = float.Parse(m_TaskParams.GetParam("LCMS2DPlotMinIntensity"));
+				m_MsFileScanner.LCMS2DOverviewPlotDivisor = int.Parse(m_TaskParams.GetParam("LCMS2DOverviewPlotDivisor"));
+
+				// Get the input file name
+				string inpFileName = GetDataFileOrFolderName(sourceFolder);
+				if (inpFileName == "Invalid File Type")
+				{
+					// DS quality test not implemented for this file type
+					result.CloseoutMsg = "";
+					result.CloseoutType = EnumCloseOutType.CLOSEOUT_SUCCESS;
+					result.EvalMsg = "Dataset quality test not implemented for data type " + m_TaskParams.GetParam("rawdatatype");
+					result.EvalCode = EnumEvalCode.EVAL_CODE_NOT_EVALUATED;
+					return result;
+				}
+
+				if (inpFileName == "")
+				{
+					// There was a problem with getting the file name; Details reported by called method
+					result.CloseoutMsg = m_Msg;
+					result.CloseoutType = EnumCloseOutType.CLOSEOUT_FAILED;
+					return result;
+				}
+
+				// Make the output folder
+				if (!Directory.Exists(outputFolder))
+				{
+					try
+					{
+						Directory.CreateDirectory(outputFolder);
+						m_Msg = "clsPluginMain.RunMsFileInfoScanner: Created output folder " + outputFolder;
+						clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.DEBUG, m_Msg);
+					}
+					catch (Exception ex)
+					{
+						m_Msg = "clsPluginMain.RunMsFileInfoScanner: Exception creating output folder " + outputFolder;
+						clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.ERROR, m_Msg, ex);
+						result.CloseoutMsg = "Exception creating output folder " + outputFolder;
+						result.CloseoutType = EnumCloseOutType.CLOSEOUT_FAILED;
+						return result;
+					}
+				}
+				
+				// Call the file scanner DLL
+				clsMSFileInfoScanner.eMSFileScannerErrorCodes errorCode;
+				bool success;
+
+				m_ErrOccurred = false;
+				success = m_MsFileScanner.ProcessMSFileOrFolder(Path.Combine(sourceFolder, inpFileName), outputFolder);
+				if (success)
+				{
+					success = m_MsFileScanner.PostDatasetInfoUseDatasetID(int.Parse(m_TaskParams.GetParam("Dataset_ID")),
+												m_MgrParams.GetParam("connectionstring"), MS_FILE_SCANNER_DS_INFO_SP);
+					if (success)
+					{
+						errorCode = clsMSFileInfoScanner.eMSFileScannerErrorCodes.NoError;
+					}
+					else
+					{
+						errorCode = m_MsFileScanner.ErrorCode;
+						m_Msg = m_Msg = "clsPluginMain.RunMsFileInfoScanner: Error running info scanner. Message = " +
+										m_MsFileScanner.GetErrorMessage() + " Result code = " + ((int)m_MsFileScanner.ErrorCode).ToString();
+						clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.ERROR, m_Msg);
+					}
+
+					if ((errorCode != clsMSFileInfoScanner.eMSFileScannerErrorCodes.NoError) || m_ErrOccurred)
+					{
+						// Either a bad result code was returned, or an error event was received
+						result.CloseoutMsg = "Job " + m_TaskParams.GetParam("Job") + ", Step " + m_TaskParams.GetParam("Step") +
+									": MSFileInfoScanner error.";
+						result.CloseoutType = EnumCloseOutType.CLOSEOUT_FAILED;
+					}
+					else
+					{
+						// Everything went wonderfully
+						result.CloseoutMsg = "Job " + m_TaskParams.GetParam("Job") + ", Step " + m_TaskParams.GetParam("Step") +
+									"completed successfully";
+						result.CloseoutType = EnumCloseOutType.CLOSEOUT_SUCCESS;
+					}
+				}
+
+				return result;
+			}	// End sub
+
+			/// <summary>
+			/// Returns the file or folder name for specified dataset based on dataset type
+			/// </summary>
+			/// <returns>Data file or folder name</returns>
+			private string GetDataFileOrFolderName(string inputFolder)
+			{
+				string fileName;
+
+				// Get the expected file name based on the dataset type
+				switch (m_TaskParams.GetParam("rawdatatype"))
+				{
+					case "dot_raw_files":
+						fileName = m_TaskParams.GetParam("dataset") + ".raw";
+						break;
+					case "zipped_s_folders":
+						fileName = "analysis.baf";
+						break;
+					default:
+						m_Msg = "clsPluginMain.GetDataFileOrFolderName: Data type " + m_TaskParams.GetParam("rawdatatype") +
+									" not recognized";
+						clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.WARN, m_Msg);
+						m_Msg = "Data type " + m_TaskParams.GetParam("rawdatatype") + " not recognized";
+						return "Invalid File Type";
+				}
+
+				// Test to verify file exists
+				if (!File.Exists(Path.Combine(inputFolder, fileName)))
+				{
+					m_Msg = "clsPluginMain.GetDataFileOrFolderName: File " + Path.Combine(inputFolder, fileName) + " not found";
+					clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.ERROR, m_Msg);
+					m_Msg = "File " + Path.Combine(inputFolder, fileName) + " not found";
+					fileName = "";
+				}
+
+				return fileName;
+			}	// End sub
+		#endregion
+
+		#region "Event handlers"
+			/// <summary>
+			/// Handles message event from MS file scanner
+			/// </summary>
+			/// <param name="Message">Event message</param>
+			void m_MsFileScanner_MessageEvent(string Message)
+			{
+				m_Msg = "clsPluginMain.RunMsFileInfoScanner: Message from MSFileInfoScanner = " + Message;
+				clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.DEBUG, m_Msg);
+			}	// End sub
+
+			/// <summary>
+			/// Handles error event from MS file scanner
+			/// </summary>
+			/// <param name="Message">Error message</param>
+			void m_MsFileScanner_ErrorEvent(string Message)
+			{
+				m_ErrOccurred = true;
+				m_Msg = "clsPluginMain.RunMsFileInfoScanner: Error running MSFileInfoScanner. Error message = " + Message;
+				clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.ERROR, m_Msg);
 			}	// End sub
 		#endregion
 	}	// End class
