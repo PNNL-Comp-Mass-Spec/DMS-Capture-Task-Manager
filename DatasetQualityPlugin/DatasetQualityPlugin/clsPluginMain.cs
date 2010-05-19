14b3d545e7c4af9959f029314a5d1e367778b09e
Dave Clark <Dave.Clark@pnnl.gov>
2010-05-19 18:20:04 +0000

    Updated Dataset Quality for processing of IMS files

--- DatasetQualityPlugin/DatasetQualityPlugin/clsPluginMain.cs
+++ DatasetQualityPlugin/DatasetQualityPlugin/clsPluginMain.cs
@@ -229,6 +229,9 @@ namespace DatasetQualityPlugin
 					case "zipped_s_folders":
 						fileName = "analysis.baf";
 						break;
+					case "dot_uimf_files":
+						fileName = m_TaskParams.GetParam("dataset") + ".uimf";
+						break;
 					default:
 						m_Msg = "clsPluginMain.GetDataFileOrFolderName: Data type " + m_TaskParams.GetParam("rawdatatype") +
 									" not recognized";
