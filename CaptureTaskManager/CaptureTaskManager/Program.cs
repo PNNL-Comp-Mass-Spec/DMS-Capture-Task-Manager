d44bdd365f686065dffebaef9536377a7ccfdd7f
alchemistmatt <matthew.monroe@pnnl.gov>
2017-07-21 18:32:29 -0700

    Update the program date

--- CaptureTaskManager/CaptureTaskManager/Program.cs
+++ CaptureTaskManager/CaptureTaskManager/Program.cs
@@ -17,7 +17,7 @@ namespace CaptureTaskManager
         // Application startup program
         //**********************************************************************************************************
 
-        private const string PROGRAM_DATE = "July 20, 2017";
+        private const string PROGRAM_DATE = "July 21, 2017";
 
         private static bool mCodeTestMode;
         private static bool mCreateEventLog;
