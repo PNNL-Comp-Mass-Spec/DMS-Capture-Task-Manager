6baaf1da487b6cee6ee9959fca1dab0b1f3a496e
alchemistmatt <matthew.monroe@pnnl.gov>
2017-09-26 13:29:30 -0700

    Update program version and update MSFileInfoScannerInterfaces.dll

--- CaptureTaskManager/CaptureTaskManager/Program.cs
+++ CaptureTaskManager/CaptureTaskManager/Program.cs
@@ -15,7 +15,7 @@ namespace CaptureTaskManager
     /// </summary>
     static class Program
     {
-        private const string PROGRAM_DATE = "August 22, 2017";
+        private const string PROGRAM_DATE = "September 21, 2017";
 
         private static bool mCodeTestMode;
         private static bool mCreateEventLog;
