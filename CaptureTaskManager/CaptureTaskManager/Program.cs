ea46d5edd8f88ea1401ba2275d4e9dd705a237de
alchemistmatt <matthew.monroe@pnnl.gov>
2018-10-15 19:21:35 -0700

    Update date and add dictionary words

--- CaptureTaskManager/CaptureTaskManager/Program.cs
+++ CaptureTaskManager/CaptureTaskManager/Program.cs
@@ -26,7 +26,7 @@ namespace CaptureTaskManager
     /// </summary>
     static class Program
     {
-        private const string PROGRAM_DATE = "October 10, 2018";
+        private const string PROGRAM_DATE = "October 15, 2018";
 
         private static bool mCodeTestMode;
         private static bool mTraceMode;
