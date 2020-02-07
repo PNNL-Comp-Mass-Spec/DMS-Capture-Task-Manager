a5c6556c0fffe0b40a0880a4cb319df6c9c6eade
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2020-02-07 13:26:19 -0800

    Update to compile against .NET 4.7.2

--- CaptureTaskManager/CaptureTaskManager/Properties/Resources.Designer.cs
+++ CaptureTaskManager/CaptureTaskManager/Properties/Resources.Designer.cs
@@ -8,8 +8,8 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CaptureTaskManager.Properties
-{
+namespace CaptureTaskManager.Properties {
+    using System;
 
 
     /// <summary>
@@ -19,7 +19,7 @@ namespace CaptureTaskManager.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
