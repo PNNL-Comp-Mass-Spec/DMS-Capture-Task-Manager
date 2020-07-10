8192dc2c6762af9cb3b07334a1d1cea0d1e270a5
alchemistmatt <matthew.monroe@pnnl.gov>
2020-07-09 17:19:45 -0700

    Update DLL and program date

--- CaptureTaskManager/CaptureTaskManager/Program.cs
+++ CaptureTaskManager/CaptureTaskManager/Program.cs
@@ -23,7 +23,7 @@ namespace CaptureTaskManager
     /// </summary>
     static class Program
     {
-        private const string PROGRAM_DATE = "June 15, 2020";
+        private const string PROGRAM_DATE = "July 9, 2020";
 
         private static bool mTraceMode;
 
