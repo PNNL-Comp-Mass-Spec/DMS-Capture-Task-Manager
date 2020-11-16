9ffb6f790c4542e2fdcdaac99f34d236d3aa0411
alchemistmatt <matthew.monroe@pnnl.gov>
2020-11-16 14:38:31 -0800

    Use foreach instead of GetEnumerator

--- CaptureTaskManager/CaptureTaskManager/clsCodeTest.cs
+++ CaptureTaskManager/CaptureTaskManager/clsCodeTest.cs
@@ -25,55 +25,51 @@ namespace CaptureTaskManager
 
             try
             {
-                using (var enumCurrent = credentials.GetEnumerator())
+                foreach (var item in credentials)
                 {
-                    while (enumCurrent.MoveNext())
+                    var accessCredentials = new System.Net.NetworkCredential(item.Key, item.Value, string.Empty);
+
+                    Console.WriteLine("Credentials created for " + item.Key);
+
+                    var bionetConnection = new NetworkConnection(sharePath, accessCredentials);
+
+                    Console.WriteLine("Connected to share");
+
+                    var shareDirectory = new System.IO.DirectoryInfo(sharePath);
+
+                    Console.WriteLine("Instantiated DirectoryInfo object: " + shareDirectory.FullName);
+
+                    var iteration = 0;
+                    foreach (var remoteFile in shareDirectory.GetFiles())
                     {
-                        var accessCredentials = new System.Net.NetworkCredential(enumCurrent.Current.Key,
-                                                                                 enumCurrent.Current.Value, string.Empty);
+                        Console.WriteLine("File: " + remoteFile.Name + " size " + remoteFile.Length + " bytes");
+                        ++iteration;
 
-                        Console.WriteLine("Credentials created for " + enumCurrent.Current.Key);
-
-                        var bionetConnection = new NetworkConnection(sharePath, accessCredentials);
-
-                        Console.WriteLine("Connected to share");
-
-                        var shareDirectory = new System.IO.DirectoryInfo(sharePath);
-
-                        Console.WriteLine("Instantiated DirectoryInfo object: " + shareDirectory.FullName);
-
-                        var iteration = 0;
-                        foreach (var remoteFile in shareDirectory.GetFiles())
+                        if (iteration > 20)
                         {
-                            Console.WriteLine("File: " + remoteFile.Name + " size " + remoteFile.Length + " bytes");
-                            ++iteration;
-
-                            if (iteration > 20)
-                            {
-                                break;
-                            }
+                            break;
                         }
-
-                        Console.WriteLine("Files Done");
-
-                        iteration = 0;
-                        foreach (var subdirectory in shareDirectory.GetDirectories())
-                        {
-                            Console.WriteLine("Folder: " + subdirectory.Name + " modified " +
-                                              subdirectory.LastWriteTime.ToString(CultureInfo.InvariantCulture));
-                            ++iteration;
-
-                            if (iteration > 20)
-                            {
-                                break;
-                            }
-                        }
-
-                        Console.WriteLine("Folders Done");
-
-                        // Disconnect network connection
-                        bionetConnection.Dispose();
                     }
+
+                    Console.WriteLine("Files Done");
+
+                    iteration = 0;
+                    foreach (var subdirectory in shareDirectory.GetDirectories())
+                    {
+                        Console.WriteLine("Folder: " + subdirectory.Name + " modified " +
+                                          subdirectory.LastWriteTime.ToString(CultureInfo.InvariantCulture));
+                        ++iteration;
+
+                        if (iteration > 20)
+                        {
+                            break;
+                        }
+                    }
+
+                    Console.WriteLine("Folders Done");
+
+                    // Disconnect network connection
+                    bionetConnection.Dispose();
                 }
             }
             catch (Exception ex)
