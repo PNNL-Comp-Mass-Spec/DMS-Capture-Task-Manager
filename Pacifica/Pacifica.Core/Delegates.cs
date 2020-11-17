6fcaf39eb27a1d444b5a6444b1e7b7e8a76f1f9e
alchemistmatt <matthew.monroe@pnnl.gov>
2020-11-17 09:58:48 -0800

    Remove unused delegates

--- Pacifica/Pacifica.Core/Delegates.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Pacifica.Core
-{
-    public delegate void MessageEventHandler(object sender, MessageEventArgs e);
-
-    public delegate void StatusUpdateEventHandler(object sender, StatusEventArgs e);
-
-    public delegate void UploadCompletedEventHandler(object sender, UploadCompletedEventArgs e);
-}
