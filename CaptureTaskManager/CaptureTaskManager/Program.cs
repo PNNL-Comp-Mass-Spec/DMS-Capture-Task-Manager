7329ce198e7f45fbc10f74c5cf7e79a8ddd3c710
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2021-02-10 16:34:12 -0800

    Updated Pacifica DLLs - fix an incomplete error report.

--- CaptureTaskManager/CaptureTaskManager/Program.cs
+++ CaptureTaskManager/CaptureTaskManager/Program.cs
@@ -25,7 +25,7 @@ namespace CaptureTaskManager
     {
         // Ignore Spelling: OxyPlot
 
-        private const string PROGRAM_DATE = "February 8, 2021";
+        private const string PROGRAM_DATE = "February 10, 2021";
 
         private static bool mTraceMode;
 
