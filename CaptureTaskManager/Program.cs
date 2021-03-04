ac936a337753bce947a290505f574eedb85d6094
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2021-03-03 21:45:12 -0800

    Update program date

--- CaptureTaskManager/Program.cs
+++ CaptureTaskManager/Program.cs
@@ -25,7 +25,7 @@ namespace CaptureTaskManager
     {
         // Ignore Spelling: OxyPlot
 
-        private const string PROGRAM_DATE = "February 26, 2021";
+        private const string PROGRAM_DATE = "March 03, 2021";
 
         private static bool mTraceMode;
 
