1c1a872687015fc86800920fe0417e9da3f77e8a
alchemistmatt <matthew.monroe@pnnl.gov>
2021-03-18 16:26:06 -0700

    Suppress warning and update program date

--- ImsDemuxPlugin/GlobalSuppressions.cs
+++ ImsDemuxPlugin/GlobalSuppressions.cs
@@ -5,6 +5,10 @@
 
 using System.Diagnostics.CodeAnalysis;
 
+[assembly: SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "Not unnecessary", Scope = "member", Target = "~M:ImsDemuxPlugin.PluginMain.DemuxTools_DemuxProgress(System.Single)")]
+[assembly: SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "Not unnecessary", Scope = "member", Target = "~M:ImsDemuxPlugin.PluginMain.RunTool~CaptureTaskManager.ToolReturnData")]
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not required", Scope = "member", Target = "~M:ImsDemuxPlugin.PluginMain.DemuxTools_BinCentricTableProgress(System.Single)")]
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not required", Scope = "member", Target = "~M:ImsDemuxPlugin.PluginMain.DemuxTools_DemuxProgress(System.Single)")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Acceptable name", Scope = "member", Target = "~M:ImsDemuxPlugin.PluginMain.DemuxTools_BinCentricTableProgress(System.Single)")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Acceptable name", Scope = "member", Target = "~M:ImsDemuxPlugin.PluginMain.DemuxTools_CopyFileWithRetryEvent(System.String)")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Acceptable name", Scope = "member", Target = "~M:ImsDemuxPlugin.PluginMain.DemuxTools_DemuxProgress(System.Single)")]
@@ -12,5 +16,3 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Acceptable name", Scope = "type", Target = "~T:ImsDemuxPlugin.PluginMain")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Acceptable name", Scope = "type", Target = "~T:ImsDemuxPlugin.SQLiteTools")]
 [assembly: SuppressMessage("Usage", "RCS1246:Use element access.", Justification = "Prefer to use .First()", Scope = "module")]
-[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not required", Scope = "member", Target = "~M:ImsDemuxPlugin.PluginMain.DemuxTools_BinCentricTableProgress(System.Single)")]
-[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not required", Scope = "member", Target = "~M:ImsDemuxPlugin.PluginMain.DemuxTools_DemuxProgress(System.Single)")]
