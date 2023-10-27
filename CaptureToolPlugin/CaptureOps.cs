b6c2d90f283d3559b066ca1c7ad28ce751800a81
alchemistmatt <matthew.monroe@pnnl.gov>
2023-10-26 19:56:54 -0700

    Remove unused using directives

--- CaptureToolPlugin/CaptureOps.cs
+++ CaptureToolPlugin/CaptureOps.cs
@@ -9,8 +9,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using CaptureTaskManager;
 using CaptureToolPlugin.DataCapture;
 using PRISM;
--- CaptureToolPlugin/PluginMainLC.cs
+++ CaptureToolPlugin/PluginMainLC.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CaptureToolPlugin
+﻿namespace CaptureToolPlugin
 {
     /// <summary>
     /// Dataset capture plugin, overridden to set a flag for LC data capture
