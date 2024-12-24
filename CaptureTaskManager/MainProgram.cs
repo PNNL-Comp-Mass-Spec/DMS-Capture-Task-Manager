2b9f2d3d43a7820990f5529eeda907fad0bc50f4
alchemistmatt <matthew.monroe@pnnl.gov>
2024-12-24 14:26:36 -0800

    Use fully-qualified class name due to ambiguity

--- CaptureTaskManager/MainProgram.cs
+++ CaptureTaskManager/MainProgram.cs
@@ -113,7 +113,7 @@ namespace CaptureTaskManager
 
             DatabaseLogger dbLogger = databaseType switch
             {
-                DbServerTypes.MSSQLServer => new SQLServerDatabaseLogger(),
+                DbServerTypes.MSSQLServer => new PRISMDatabaseUtils.Logging.SQLServerDatabaseLogger(),
                 DbServerTypes.PostgreSQL => new PostgresDatabaseLogger(),
                 _ => throw new Exception("Unsupported database connection string: should be SQL Server or Postgres")
             };
